e908749b38791a8fc6d4fc8129cfb1fc4e46dcda
Aleksandur Dimitrov <52452769+aleks247@users.noreply.github.com>
2025-10-28 22:39:51 +0200

    Adding the Config file

---
+++ Config.py
@@ -0,0 +1,10 @@
+# --- CONFIG ---
+IFTTT_URL = "YOUR API KEY HERE"
+CHECK_INTERVAL = 300  #5 minutes
+LOG_FOLDER = "price_logs"
+
+# --- SKINS TO TRACK (EUR) ---
+SKINS = {
+    "SSG 08 | Dragonfire (Field-Tested)": "https://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name=SSG%2008%20%7C%20Dragonfire%20%28Field-Tested%29",
+    "M4A1-S | Cyrex (Field-Tested)": "https://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name=M4A1-S%20%7C%20Cyrex%20%28Field-Tested%29"
+}
